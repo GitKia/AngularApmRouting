c0f9e7b810f7bfbf5011febcdfcbb39a7c34f3ac
KiaZA91 <111753874+KiaZA91@users.noreply.github.com>
2023-07-16 17:42:02 +0200

    TemplateAdded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { MessageModule } from './messages/message.module';
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    RouterModule.forRoot([git br
+    RouterModule.forRoot([
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', component: PageNotFoundComponent}
