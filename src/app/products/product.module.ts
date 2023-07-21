7f1bf055d72b644123e71d32bf33e3b70bfd5c76
KiaZA91 <111753874+KiaZA91@users.noreply.github.com>
2023-07-21 13:14:01 +0200

    child-pathAdded

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,25 +8,45 @@ import { ProductService } from './product.service';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', component: ProductDetailComponent,
-      resolve: { resolvedData: ProductResolver}
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver}
       },
-      {path: 'products/:id/edit',
-      component: ProductEditComponent,
-      resolve: {resolvedData: ProductResolver}
-    }
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: {resolvedData: ProductResolver},
+        children: [
+          {
+            path:'',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
