d9b5b44040db7e2d432f4dd7c63c27139a5af3dc
KiaZA91 <111753874+KiaZA91@users.noreply.github.com>
2023-07-24 18:06:21 +0200

    show/hidePopupAdded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel} from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 import { AuthService } from './user/auth.service';
 import { animation } from '@angular/animations';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -26,23 +27,33 @@ export class AppComponent {
   }
 
   constructor(private authService: AuthService,
-    private router: Router) {
-      router.events.subscribe((routerEvent : Event) => {
-        this.checkRouterEvent(routerEvent);
-      });
-     }
+    private router: Router,
+    public messageService: MessageService) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
 
-     checkRouterEvent(routerEvent: Event): void {
-      if (routerEvent instanceof NavigationStart) {
-        this.loading = true;
-      }
-  
-      if (routerEvent instanceof NavigationEnd ||
-          routerEvent instanceof NavigationCancel ||
-          routerEvent instanceof NavigationError) {
-        this.loading = false;
-      }
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
     }
+
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+
+  public displayMessages(): void {
+    this.router.navigate([{ outlets: { popup: ['messages']}}]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.messageService.isDisplayed = false;
+  }
 
   logOut(): void {
     this.authService.logout();
